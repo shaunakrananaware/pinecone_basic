bf18be12615f49232255f2a448ec8064a31722f6
Shaunak Rananaware <113698409+shaunakrananaware@users.noreply.github.com>
2023-06-16 20:41:31 +0530

    Update DPApinecone.py

--- DPApinecone.py
+++ DPApinecone.py
@@ -4,3 +4,6 @@ pinecone.init(api_key="8e480581-80e0-4c84-82b3-7ec344fba470",
 #create index
 pinecone.create_index("firstindex", dimension=4, metric="euclidean")
 print("INDEXS: ", pinecone.list_indexes(), "\n\n")
+
+#delete index
+pinecone.delete_index("firstindex")
