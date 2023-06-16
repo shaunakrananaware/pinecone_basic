50b1f3500507795eb95c8db5bf62ee1c738eb6e9
Shaunak Rananaware <113698409+shaunakrananaware@users.noreply.github.com>
2023-06-16 20:42:03 +0530

    Update DPApinecone.py

--- DPApinecone.py
+++ DPApinecone.py
@@ -1,9 +1,19 @@
 import pinecone
 pinecone.init(api_key="8e480581-80e0-4c84-82b3-7ec344fba470",
               environment="us-west4-gcp-free")
-#create index
+# create index
 pinecone.create_index("firstindex", dimension=4, metric="euclidean")
 print("INDEXS: ", pinecone.list_indexes(), "\n\n")
 
-#delete index
+# delete index
 pinecone.delete_index("firstindex")
+
+# insert data into index
+index = pinecone.Index("firstindex")
+index.upsert([
+    ("A", [0.1, 0.1, 0.1, 0.1]),
+    ("B", [0.2, 0.2, 0.2, 0.2]),
+    ("C", [0.3, 0.3, 0.3, 0.3]),
+    ("D", [0.4, 0.4, 0.4, 0.4]),
+    ("E", [0.5, 0.5, 0.5, 0.5])
+])
