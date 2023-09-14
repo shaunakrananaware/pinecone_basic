a44b12cb13b352df5c964ebdd6fd7d80ec190c4f
Shaunak Rananaware <113698409+shaunakrananaware@users.noreply.github.com>
2023-09-14 20:21:07 +0530

    Update DPApinecone.py

--- DPApinecone.py
+++ DPApinecone.py
@@ -1,5 +1,5 @@
 import pinecone
-pinecone.init(api_key="8e480581-80e0-4c84-82b3-7ec344fba470",
+pinecone.init(api_key="<ENTER_YOUR_API_KEY_HERE>",
               environment="us-west4-gcp-free")
 # create index
 pinecone.create_index("firstindex", dimension=4, metric="euclidean")
